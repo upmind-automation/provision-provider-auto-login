a5955f69913e107e1fe7b1bd3b5a4e05cad43c86
Harry Lewis <harry@upmind.com>
2023-05-25 16:02:24 +0100

    Update LaravelServiceProvider

--- src/LaravelServiceProvider.php
+++ src/LaravelServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Upmind\ProvisionProviders\AutoLogin;
 
 use Upmind\ProvisionBase\Laravel\ProvisionServiceProvider;
+use Upmind\ProvisionProviders\AutoLogin\Providers\Example\Provider as ExampleProvider;
 use Upmind\ProvisionProviders\AutoLogin\Providers\Generic\Provider as GenericProvider;
 use Upmind\ProvisionProviders\AutoLogin\Providers\SpamExperts\Provider as SpamExpertsProvider;
 
@@ -13,6 +14,8 @@ class LaravelServiceProvider extends ProvisionServiceProvider
     public function boot()
     {
         $this->bindCategory('auto-login', Category::class);
+
+        // $this->bindProvider('auto-login', 'example', ExampleProvider::class);
 
         $this->bindProvider('auto-login', 'generic', GenericProvider::class);
         $this->bindProvider('auto-login', 'spam-experts', SpamExpertsProvider::class);
